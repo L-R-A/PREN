471b503838a31f1880dc372ca69607c9b5b74546
muhtob <94535391+muhtob@users.noreply.github.com>
2023-10-26 10:42:44 +0200

    Update PREN_ADC.ino
    Updated: Gemessener ADC Wert wird anhand Uref in effektiven Spannungswert umgerechnet.

--- esp/PREN_ADC/PREN_ADC.ino
+++ esp/PREN_ADC/PREN_ADC.ino
@@ -1,9 +1,15 @@
+int uRef = 3; // Reverenzspannung für ADC in V
+int uMes = 0; // Gemessener Spannungswert
+int adcVal = 0; // Gemessener Wert von ADC (0-1024)
+
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  Serial.print("ADC Value: ");
-  Serial.println(analogRead(A0));
+  adcVal = analogRead(A0);
+  uMes = uRef - (uRef/1024)*adcVal;   // Berechnung des aktuellen Spannungswerts
+  Serial.println(uMes);
+
   delay(300);
 }
