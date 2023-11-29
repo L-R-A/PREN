24242054b1818fc5c61f88a6753c37e36231a549
Noah Eggenberger <noah.n.eggenberger@gmail.com>
2023-11-29 10:07:38 +0100

    make strategy one trainable

--- video_analyse/src/using_machine_learning.py
+++ video_analyse/src/using_machine_learning.py
@@ -4,6 +4,8 @@ from PIL import Image
 import numpy as np
 # import tensorflow as tf
 import keras.api._v2.keras as keras
+import random
+
 
 
 RESSOURCES_PATH = "../tmp/train/ressources/"
@@ -15,7 +17,7 @@ STRATEGY = 2
 IN_DEBUG_MODE = True
 JSON_NAME = "scene_results.json"
 
-TRAIN_MODEL = True
+TRAIN_MODEL = False
 
 IMAGE_HEIGHT_PX = 180
 IMAGE_WIDTH_PX = 320
@@ -29,6 +31,7 @@ NORMALIZE_VALUE = 255
 LOSS_FUNCTION = 'categorical_crossentropy'
 
 color_mapping = { 'red': 0, 'yellow': 1, 'blue': 2, '': 3 }
+label_mapping = { 0: 'red', 1: 'yellow', 2: 'blue', 3: ''}
 
 def get_data(stage):
     labels = []
@@ -44,24 +47,35 @@ def get_data(stage):
     with open(scene_results_path, 'r') as file:
         scene_results = json.load(file)
 
+    
+
     for result in scene_results:
         img = [];
+
+        image_not_found = False
 
         for img_path in result["imagePaths"]:
 
             if IN_DEBUG_MODE:
                 print("READING IMAGE AT: " + img_path)
 
-            i = Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), RESSOURCES_PATH, TRAIN_DATA_FOLDER, img_path))
+            try:
+                i = Image.open(os.path.join(os.path.dirname(os.path.abspath(__file__)), RESSOURCES_PATH, TRAIN_DATA_FOLDER, img_path))
+            except:
+                image_not_found = True
+                continue
             
 
             # Scale down image (resize)
             i = i.resize((IMAGE_WIDTH_PX, IMAGE_HEIGHT_PX))
-
-            img.append(i)    
-
-        images.append(img)
-        labels.append(result["positions"])
+            if STRATEGY == 2:
+                img.append(i)
+            else:
+                img = i
+                    
+        if image_not_found == False:
+            images.append(img)
+            labels.append(result["positions"])
 
     if IN_DEBUG_MODE:
         print("\n\n")
@@ -90,7 +104,17 @@ def get_base_model():
         keras.layers.Dense(128, activation='relu'),
     ])
 
-def get_complete_model(base_model):
+def setup_strategy_one_model(base_model):
+    input_image = keras.layers.Input(shape=(IMAGE_HEIGHT_PX, IMAGE_WIDTH_PX, NUM_CHANNELS), name='input_image_1')
+
+    input = base_model(input_image)
+
+    dense_layer = keras.layers.Dense(NUM_POSITIONS * NUM_CLASSES, activation='softmax')(input)
+    reshaped_output = keras.layers.Reshape((NUM_POSITIONS, NUM_CLASSES))(dense_layer)
+
+    return keras.models.Model(inputs=[input_image], outputs=reshaped_output)
+
+def setup_strategy_two_model(base_model):
     input_image_1 = keras.layers.Input(shape=(IMAGE_HEIGHT_PX, IMAGE_WIDTH_PX, NUM_CHANNELS), name='input_image_1')
     input_image_2 = keras.layers.Input(shape=(IMAGE_HEIGHT_PX, IMAGE_WIDTH_PX, NUM_CHANNELS), name='input_image_2')
 
@@ -129,9 +153,16 @@ def create_model():
     model = get_base_model()
     # model = get_siamese_network(base_model)
 
-    model = get_complete_model(model)
+    if STRATEGY == 1:
+        model = setup_strategy_one_model(model)
+    
+    if STRATEGY == 2:
+        model = setup_strategy_two_model(model)
 
     optimizer = keras.optimizers.legacy.Adam(learning_rate=0.001)
+
+
+    # model.compile(optimizer=optimizer, loss=LOSS_FUNCTION, metrics=['mean_squared_error'])
 
     model.compile(optimizer=optimizer, loss=LOSS_FUNCTION, metrics=['accuracy'])
 
@@ -144,14 +175,15 @@ def create_model():
 def test_model(model, test_data):
     test_images = normalize_images(np.array((test_data[0])))
 
-    return model.predict([test_images[:, 0], test_images[:, 1]])
+
+    if STRATEGY == 1:
+        return model.predict(test_images)
+    
+    if STRATEGY == 2:
+        return model.predict([test_images[:, 0], test_images[:, 1]])
 
 
-
-def train_model():
-    train_data = get_data("Train")
-    verify_data = get_data("Verify")
-
+def fit_model(model, train_data, verify_data):
     train_images = normalize_images(np.array(train_data[0]))
     numberic_train_labels = np.array([map_labels_to_nummeric(label) for label in train_data[1]])
     train_labels = keras.utils.to_categorical(numberic_train_labels, num_classes=NUM_CLASSES)
@@ -160,23 +192,43 @@ def train_model():
     numberic_verify_labels = np.array([map_labels_to_nummeric(label) for label in verify_data[1]])
     verify_labels = keras.utils.to_categorical(numberic_verify_labels, num_classes=NUM_CLASSES)
 
-
-    model = create_model()
-
     if IN_DEBUG_MODE: 
         print("----- SHAPES ------\n")
         print(f"Train labels shape: {train_labels.shape}")
         print(f"Train images shape: {train_images.shape}")
 
         print(f"Verify labels shape: {verify_labels.shape}")
-        print(f"Verify images shape: {verify_images.shape}")
+        print(f"Verify images shape: {verify_images.shape}\n\n")
 
-    model.fit(
-        [train_images[:, 0], train_images[:, 1]], train_labels, 
-        epochs=10, 
-        validation_data=([verify_images[:, 0], verify_images[:, 1]], verify_labels), verbose= 1 if IN_DEBUG_MODE else 0)
+    if STRATEGY == 1:
+         model.fit(
+            train_images, train_labels, 
+            epochs=10, 
+            validation_data=(verify_images, verify_labels), verbose= 1 if IN_DEBUG_MODE else 0)
 
+    if STRATEGY == 2:
+        model.fit(
+            [train_images[:, 0], train_images[:, 1]], train_labels, 
+            epochs=10, 
+            validation_data=([verify_images[:, 0], verify_images[:, 1]], verify_labels), verbose= 1 if IN_DEBUG_MODE else 0)
+        
     return model
+
+
+def train_model():
+    train_data = get_data("Train")
+    verify_data = get_data("Verify")
+    model = create_model()
+    model = fit_model(model, train_data, verify_data)
+    return model
+
+def map_back_label(nummeric_label):
+    mapped_label = []
+
+    for pos in nummeric_label:
+        mapped_label.append(color_mapping[pos])
+
+    return mapped_label
 
 
 
@@ -195,15 +247,30 @@ def main():
 
 
     if IN_DEBUG_MODE:
-        print("\n\n")
-        print("------ PREDICTIONS --------\n\n")
-        predicted_indices = np.argmax(predictions, axis=-1)
-        print(predicted_indices)
+        label_index = random.randint(0, len(test_labels)-1)
 
         print("\n\n")
+        print(f"------ PREDICTION: Index {label_index} --------\n")
+        predicted_nummeric = np.argmax(predictions, axis=-1)
+        predicted_readable = np.vectorize(label_mapping.get)(predicted_nummeric)
 
-        print("------ ACTUAL RESULTS ------ \n\n")
-        print(test_labels)
+        actual_readable = np.vectorize(label_mapping.get)(test_labels)
+
+
+        print("NUMMERIC: \n")
+        print(predicted_nummeric[label_index])
+        print("READABLE: \n")
+        print(predicted_readable[label_index])
+        print("\n\n")
+
+        print(f"------ ACTUAL: Index {label_index} ------ \n")
+        print("NUMMERIC: \n")
+        print(test_labels[label_index])
+        print("READABLE: \n")
+        print(actual_readable[label_index])
+
+
+        print()
 
 
     return
