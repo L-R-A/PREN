66a0d425284dcd710bbf9264c336cded202fca17
Raspberry Pren Luzian <luzian.a@icloud.com>
2024-03-25 16:10:27 +0100

    added Logininitialisation

--- display/display.py
+++ display/display.py
@@ -40,150 +40,95 @@
 #---------------------------------------------------------
 
 
-import board
-import RPi.GPIO as GPIO
+from displaylib import LCD_driver as LCD
 import time
+import time
+#import datetime
+import board
+import busio
+import digitalio
+#import RPi.GPIO as GPIO
+from displaylib import LCD_driver as LCD
+from adafruit_servokit import ServoKit
+from adafruit_motorkit import MotorKit
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
-# Define GPIO to LCD mapping
-LCD_RS = 9
-LCD_E  = 10
-LCD_D4 = 16
-LCD_D5 = 19
-LCD_D6 = 20
-LCD_D7 = 21
+#initialize LCD
+LCD.init()
 
-# Define some device constants
-LCD_WIDTH = 16    # Maximum characters per line
-LCD_CHR = True
-LCD_CMD = False
- 
-LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
-LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
- 
-# Timing constants
-E_PULSE = 0.0005
-E_DELAY = 0.0005
- 
-def main():
-  # Main program block
-  GPIO.setwarnings(False)
-  GPIO.setmode(GPIO.BCM)       # Use BCM GPIO numbers
-  GPIO.setup(LCD_E, GPIO.OUT)  # E
-  GPIO.setup(LCD_RS, GPIO.OUT) # RS
-  GPIO.setup(LCD_D4, GPIO.OUT) # DB4
-  GPIO.setup(LCD_D5, GPIO.OUT) # DB5
-  GPIO.setup(LCD_D6, GPIO.OUT) # DB6
-  GPIO.setup(LCD_D7, GPIO.OUT) # DB7
- 
-  # Initialise display
-  lcd_init()
- 
-  while True:
- 
-    # Send some test
-    lcd_string("Rasbperry Pi",LCD_LINE_1)
-    lcd_string("16x2 LCD Test",LCD_LINE_2)
- 
-    time.sleep(3) # 3 second delay
- 
-    # Send some text
-    lcd_string("1234567890123456",LCD_LINE_1)
-    lcd_string("abcdefghijklmnop",LCD_LINE_2)
- 
-    time.sleep(3) # 3 second delay
- 
-    # Send some text
-    lcd_string("RaspberryPi-spy",LCD_LINE_1)
-    lcd_string(".co.uk",LCD_LINE_2)
- 
-    time.sleep(3)
- 
-    # Send some text
-    lcd_string("Follow me on",LCD_LINE_1)
-    lcd_string("Twitter @RPiSpy",LCD_LINE_2)
- 
-    time.sleep(3)
- 
-def lcd_init():
-  # Initialise display
-  lcd_byte(0x33,LCD_CMD) # 110011 Initialise
-  lcd_byte(0x32,LCD_CMD) # 110010 Initialise
-  lcd_byte(0x06,LCD_CMD) # 000110 Cursor move direction
-  lcd_byte(0x0C,LCD_CMD) # 001100 Display On,Cursor Off, Blink Off
-  lcd_byte(0x28,LCD_CMD) # 101000 Data length, number of lines, font size
-  lcd_byte(0x01,LCD_CMD) # 000001 Clear display
-  time.sleep(E_DELAY)
- 
-def lcd_byte(bits, mode):
-  # Send byte to data pins
-  # bits = data
-  # mode = True  for character
-  #        False for command
- 
-  GPIO.output(LCD_RS, mode) # RS
- 
-  # High bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x10==0x10:
-    GPIO.output(LCD_D4, True)
-  if bits&0x20==0x20:
-    GPIO.output(LCD_D5, True)
-  if bits&0x40==0x40:
-    GPIO.output(LCD_D6, True)
-  if bits&0x80==0x80:
-    GPIO.output(LCD_D7, True)
- 
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
- 
-  # Low bits
-  GPIO.output(LCD_D4, False)
-  GPIO.output(LCD_D5, False)
-  GPIO.output(LCD_D6, False)
-  GPIO.output(LCD_D7, False)
-  if bits&0x01==0x01:
-    GPIO.output(LCD_D4, True)
-  if bits&0x02==0x02:
-    GPIO.output(LCD_D5, True)
-  if bits&0x04==0x04:
-    GPIO.output(LCD_D6, True)
-  if bits&0x08==0x08:
-    GPIO.output(LCD_D7, True)
- 
-  # Toggle 'Enable' pin
-  lcd_toggle_enable()
- 
-def lcd_toggle_enable():
-  # Toggle enable
-  time.sleep(E_DELAY)
-  GPIO.output(LCD_E, True)
-  time.sleep(E_PULSE)
-  GPIO.output(LCD_E, False)
-  time.sleep(E_DELAY)
- 
-def lcd_string(message,line):
-  # Send string to display
- 
-  message = message.ljust(LCD_WIDTH," ")
- 
-  lcd_byte(line, LCD_CMD)
- 
-  for i in range(LCD_WIDTH):
-    lcd_byte(ord(message[i]),LCD_CHR)
- 
-if __name__ == '__main__':
- 
-  try:
-    main()
-  except KeyboardInterrupt:
-    pass
-  finally:
-    lcd_byte(0x01, LCD_CMD)
-    lcd_string("Goodbye!",LCD_LINE_1)
-    GPIO.cleanup()
+LCD.string("CPU ready!", LCD.LCD_LINE_1)
+LCD.string("Initialize Tests", LCD.LCD_LINE_2)
+time.sleep(1)
+
+
+# initialize shields
+LCD.string("Initialize Servos", LCD.LCD_LINE_1)
+LCD.string("...",LCD.LCD_LINE_2)
+time.sleep(1)
+try:
+    servoKit = ServoKit(channels=16,address=0x42)
+    stepperKit = MotorKit(address=0x61,i2c=board.I2C())
+    LCD.string("Success!",LCD.LCD_LINE_2)
+except: 
+    LCD.string("ERROR!",LCD.LCD_LINE_2)
+
+# Initialize GPIO
+LCD.string("Initialize Stepper", LCD.LCD_LINE_1)
+LCD.string("...",LCD.LCD_LINE_2)
+time.sleep(1)
+try:
+    start = digitalio.DigitalInOut(board.D13)
+    start.direction = digitalio.Direction.INPUT
+    LCD.string("Success!",LCD.LCD_LINE_2)
+except: 
+    LCD.string("ERROR!",LCD.LCD_LINE_2)
 
 
 
+try:
+    LCD.string("Test Steppers", LCD.LCD_LINE_1)
+    LCD.string("...",LCD.LCD_LINE_2)
+
+    # Test Stepper
+    for i in range(400):
+            stepperKit.stepper1.onestep()
+            stepperKit.stepper2.onestep()
+
+    LCD.string("Success!",LCD.LCD_LINE_2)
+    time.sleep(1)
+
+
+    LCD.string("Test Servos", LCD.LCD_LINE_1)
+    LCD.string("...",LCD.LCD_LINE_2)
+
+
+    # Test Servo
+    servoKit.servo[0].angle = 180
+    servoKit.servo[1].angle = 180
+    servoKit.servo[2].angle = 180
+    servoKit.servo[3].angle = 180
+    #servoKit.continuous_servo[0].throttle = 1
+    #servoKit.continuous_servo[1].throttle = 1
+    time.sleep(1)
+    #servoKit.continuous_servo[0].throttle = -1
+    #servoKit.continuous_servo[1].throttle = -1
+    time.sleep(1)
+    servoKit.servo[0].angle = 0
+    servoKit.servo[1].angle = 0
+    servoKit.servo[2].angle = 0
+    servoKit.servo[3].angle = 0
+    #servoKit.continuous_servo[1].throttle = 0
+    time.sleep(1)
+    LCD.string("Success!",LCD.LCD_LINE_2)
+    time.sleep(1)
+
+except: 
+    LCD.string("ERROR",LCD.LCD_LINE_2)
+
+LCD.string("Welcome Back!",LCD.LCD_LINE_1)
+LCD.string("Systems nominal!", LCD.LCD_LINE_2)
+time.sleep(5)
+LCD.string("Skogahof", LCD.LCD_LINE_1)
+LCD.string("v0.2.1", LCD.LCD_LINE_2)
+LCD.cleanup()
