f19cb73e89ba0834dfb402c4c264bfcce172a8bc
muhtob <tobiasmuehlemann@gmx.ch>
2024-04-25 08:49:56 +0200

    grobes framework

--- MainCode/main.py
+++ MainCode/main.py
@@ -39,9 +39,12 @@ run = False
 lightIN = 17
 pi.set_mode(lightIN,pigpio.INPUT)
 energy_wh = 0
-end_position = False
-status = 'idle'
+end_position = False # for platform -> laser detection
+status = 'idle' # for status update and progress on display 
 cube_storage = ["yellow","red","blue"] # 3 = dummy
+release_cube = 90 # angle for cube storage to release cube
+turn_magazine = 100 # steps for stepper to turn 90°
+platform_move = 100 # steps for platform to move up or down
 
 def display():
     global energy_wh
@@ -82,7 +85,11 @@ def display():
                 LCD.string(str("LOWER PLATFORM"),LCD.LCD_LINE_1)
             if status == 'cube_center':
                 LCD.string(str("CENTRALIZE CUBES"),LCD.LCD_LINE_1)
-
+            if status == 'fiinished':
+                while(not pi.read(start)):
+                    LCD.string(str("FINISHED"),LCD.LCD_LINE_1)
+                    LCD.string(str(round(process,1) + "% " + str(round(end - start),0) + "s " + str(round(energy_wh,2)) + "Wh"),LCD.LCD_LINE_2)
+                
             # update status on display line 2
             LCD.string(str(round(process,1) + "% " + str(round(end - start),0) + "s " + str(round(energy_wh,2)) + "Wh"),LCD.LCD_LINE_2)
             if process < 100.0:
@@ -170,7 +177,10 @@ def main():
     except:
         LCD.string(str("I2C Err: STEPPER"),LCD.LCD_LINE_2)
         time.sleep(3)
-    servoKit.servo[4]._pwm_out
+    magazin =  stepperKit.stepper1
+    platform = stepperKit.stepper2
+    
+    #servoKit.servo[4]._pwm_out
 
     # Initialize GPIO
     start = 13
@@ -204,17 +214,21 @@ def main():
         end_position = False
         status = 'idle'
         cubes = ["","","","","","","",""] # yellow, red, blue
-        drop_char = 0 # 4 bits to indicate droping: NAV, blue, red, yellow -> 0001 = yellow drop
 
         # Default Positon Cube Push Mechanism    
         servo_push1.angle = 0
         servo_push2.angle = 0
-        stepperKit.stepper1.release()
-        stepperKit.stepper2.release()
+        magazin.release()
+        # raise platform
+        while(end_position == False): 
+            for i in range(platform_move):
+                platform.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
+            platform.release()
+        platform.release()
 
         ################## START RUN ##################
         while(not pi.read(start)):
-            time.sleep(0.1)
+            time.sleep(0.01)
         pi.write(statled,1)
 
         # start img processing
@@ -225,13 +239,6 @@ def main():
         # cube drop process
         status = 'cube_drop'
 
-        # defined cube storages in global list: 0 = yellow, 1 = red, 2 = blue, 3 = dummy
-        if cube_storage[0] == cubes [0]:
-            TODO=0
-            
-
-
-
         """
         Die Werte 1-8 bezeichnen die Positionen eines Würfels oder einer leeren Stelle in der Konfiguration.
         Die Position 1 spezifiziert die Stelle die auf dem weissen Sektor des Drehtellers, liegt die Positionen 2,
@@ -239,32 +246,69 @@ def main():
         sind diejenigen auf dem 2 Level, i.e. auf einem darunterliegenden Würfel. Ist an der entsprechenden
         Stelle kein Würfel, so ist die Position leer. Position 5 liegt auf 1, 6 auf 2, 7 auf 3 und 8 auf 4.
         """
-        
 
-        """
-        for i in range(500):
-            stepperKit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-            stepperKit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-        time.sleep(1)
-        for i in range(500):
-            stepperKit.stepper1.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-            stepperKit.stepper2.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
-            
-        stepperKit.stepper1.release()
-        stepperKit.stepper2.release()
-        """
+        # defined cube storages in global list: 0 = yellow, 1 = red, 2 = blue, 3 = dummy
+        # Turn forward
+        for i in range(0,3,1):
+            if cube_storage[0] == cubes[i]:
+                servo_yellow.angle = release_cube
+            if i <= 2:
+                if cube_storage[1] == cubes [i + 1]:
+                    servo_red.angle = release_cube
+            else:
+                if cube_storage[1] == cubes [i - 3]:
+                    servo_red.angle = release_cube
+            if i <= 1:    
+                if cube_storage[2] == cubes[i + 2]:
+                    servo_blue.angle = release_cube
+            else:
+                if cube_storage[2] == cubes[i - 2]:
+                    servo_blue.angle = release_cube
+
+            # reset servos
+            time.sleep(0.01)
+            servo_yellow.angle = 0
+            servo_red.angle = 0
+            servo_blue.angle = 0
+
+            # turn magazin for 90°
+            for i in range(turn_magazine):
+                magazin.onestep(direction=stepper.FORWARD, style = stepper.DOUBLE)
+            magazin.release()
+
+        # turn backward
+        for i in range(3,0,-1):
+            if cube_storage[0] == cubes[i]:
+                servo_yellow.angle = release_cube
+            if i <= 2:
+                if cube_storage[1] == cubes [i + 1]:
+                    servo_red.angle = release_cube
+            else:
+                if cube_storage[1] == cubes [i - 3]:
+                    servo_red.angle = release_cube
+            if i <= 1:    
+                if cube_storage[2] == cubes[i + 2]:
+                    servo_blue.angle = release_cube
+            else:
+                if cube_storage[2] == cubes[i - 2]:
+                    servo_blue.angle = release_cube
+
+            # reset servos
+            time.sleep(0.01)
+            servo_yellow.angle = 0
+            servo_red.angle = 0
+            servo_blue.angle = 0
+
+            # turn magazin for 90°
+            for i in range(turn_magazine):
+                magazin.onestep(direction=stepper.BACKWARD, style = stepper.DOUBLE)
+            magazin.release()
+
         status = 'lower_plattform'
         while(end_position == False): 
-            for i in range(50):
-                stepperKit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-                stepperKit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.DOUBLE)
-            """
-            for i in range(50):
-                stepperKit.stepper1.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
-                stepperKit.stepper2.onestep(direction=stepper.FORWARD, style=stepper.MICROSTEP)
-           """
-            stepperKit.stepper1.release()
-            stepperKit.stepper2.release()
+            for i in range(platform_move):
+                platform.onestep(direction=stepper.BACKWARD, style=stepper.DOUBLE)
+            platform.release()
         
         status = 'cube_center'
         # When Platform is low enough -> Push cubes together
@@ -274,8 +318,7 @@ def main():
         servo_push1.angle = 0
         servo_push2.angle = 0
 
-        time.sleep(1)
-
+        status = 'finished'
         # Accoustic Signal
         #pi.write(buzz,1)
         time.sleep(0.2)
